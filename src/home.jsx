7b571e106a039f56cfb0bb4ecdfd8c5689e8d161
wadegundy12 <wadegundy12@gmail.com>
2024-11-01 17:11:47 -0600

    'app will display here' displayed

---
+++ src/home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './home.css';
+
+export default function Home() {
+  return <div className='body bg-dark text-light'>App will display here</div>;
+}
