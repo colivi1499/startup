b042d0cabdf547d1ea67e704e89bee213a9e3049
Cameron Olivier <cameronolivier@Camerons-Air-7.app.byu.edu>
2024-11-14 14:32:52 -0700

    Added Not found

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Group from './group'
 import Home from './home'
@@ -11,8 +12,17 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/group" element={<Group />} />
           <Route path="/" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+    );
+  }
+  
+  function NotFound() {
+    return (
+      <main className="container-fluid bg-secondary text-center d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+        <h1 className="text-white">404: Return to sender. Address unknown.</h1>
+      </main>
     );
   }
   
